3b23bc90ec4711f65ca2c55a33754bb3f8f48777
ercekal <ercekalabalikoglu@gmail.com>
2017-01-04 14:19:01 +0000

    adds email and password inputs, does styling and secureTextEntry

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyA-Iiz952t9bG5mV5O1TF5gciBhnGsdE1w",
+      authDomain: "auth-78eb8.firebaseapp.com",
+      databaseURL: "https://auth-78eb8.firebaseio.com",
+      storageBucket: "auth-78eb8.appspot.com",
+      messagingSenderId: "78525112643"
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>This is an App!</Text>
+        <LoginForm />
       </View>
     );
   }
